68f3b395da40635f7bbec7257cc46d7f01126b26
Jp <jeanpaul.saliba@ontariotechu.net>
2025-12-04 17:31:31 -0500

    updated login page

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout.jsx'
+
 
 const Login = () => {
   return (
-    <div>
-      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify center">
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
         <p className="text-xs text-state-700 mt-[5px] mb-6">Please enter your details to log in</p>
       </div>
-    </div>
+    </AuthLayout>
   )
 }
 

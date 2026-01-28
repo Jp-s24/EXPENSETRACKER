a04be06146220ef3dc98140695ff70424579a8a1
Jp <jeanpaul.saliba@ontariotechu.net>
2026-01-28 16:03:45 -0500

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use(express.json());
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {

7e9a16993ce42887e5b48e0488c6658e4a254c3f
Jp <jeanpaul.saliba@ontariotechu.net>
2026-02-06 19:25:12 -0500

    updated and added backend for dashboard

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 const path = require("path");
 
 
@@ -21,6 +22,8 @@ app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes); 
 app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
 
 
 //Serve uploads folder 
